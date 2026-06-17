fab47294f2c9cf5033f96ded88f69ad2955bdcea
Borys Shaposhnikov <borjkaru@gmail.com>
2026-06-17 12:56:01 +0300

    Build

--- Assets/AI Context.cs
+++ Assets/AI Context.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class AIContext : MonoBehaviour
 {
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+         QualitySettings.vSyncCount = 0;
+         Application.targetFrameRate = 100;
     }
 
     // Update is called once per frame
